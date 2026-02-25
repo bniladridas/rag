039a965cea7d21531c7b08f05dbb40f371d39df7
semantic-release <semantic-release>
2026-02-25 21:55:43 +0000

    chore(release): 1.6.14

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = Path("README.asc").read_text(encoding="utf-8")
 
 setup(
     name="rag",
-    version="1.6.13",
+    version="1.6.14",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
